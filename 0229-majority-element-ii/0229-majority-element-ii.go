afdf51408894e843f2d6a3414a39a52e9ac0b698
shreeharing <79448337+shreeharing@users.noreply.github.com>
2026-07-12 19:23:37 +0530

    Time: 0 ms (100%), Space: 8.5 MB (43.3%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.go
+++ 0229-majority-element-ii/0229-majority-element-ii.go
@@ -1,16 +1,43 @@
 func majorityElement(nums []int) []int {
-    hashMap := make(map[int]int)
+    count1 := 0
+    count2 := 0
+    ele1 := math.MinInt
+    ele2 := math.MinInt
     result := []int{}
-    k := len(nums)/3
-
     for _,val := range nums {
-        hashMap[val]++
+        if count1 == 0 && ele2 != val {
+            count1 = 1
+            ele1 = val
+        }else if count2 == 0 && ele1 != val {
+            count2 = 1
+            ele2 = val
+        }else if val == ele1 {
+            count1++
+        }else if val == ele2 {
+            count2++
+        }else{
+            count1--;
+            count2--
+        }
     }
 
-    for key,val := range hashMap{
-        if val > k{
-            result = append(result,key)
+    count1 = 0
+    count2 = 0;
+
+    for _,val := range nums {
+        if val == ele1 {
+            count1++
+        }else if val == ele2 {
+            count2++
         }
+    }
+    var k int
+    k = len(nums)/3
+    if count1 > k {
+        result = append(result,ele1)
+    }
+    if count2 > k{
+        result = append(result,ele2)
     }
     return result
 }
