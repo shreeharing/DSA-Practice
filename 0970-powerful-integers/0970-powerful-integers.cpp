0daba5c61f426fc99eb1b654d58ae7e1f80bd62d
shreeharing <79448337+shreeharing@users.noreply.github.com>
2026-07-30 22:55:42 +0530

    Time: 0 ms (100%), Space: 9.2 MB (79.53%) - LeetHub

--- 0970-powerful-integers/0970-powerful-integers.cpp
+++ 0970-powerful-integers/0970-powerful-integers.cpp
@@ -1,42 +1,15 @@
 class Solution {
 public:
     vector<int> powerfulIntegers(int x, int y, int bound) {
-        set<int>ans;
-        int xLim = binarySearch(x,bound);
-        int yLim = binarySearch(y,bound);
-        
-        for(int i=0;i<=xLim;i++){
-            long long x1 = (long long) pow(x,i);
-            if(x1 > bound)break;
-            for(int j=0;j<=yLim;j++){
-                long long y1 = (long long)pow(y,j);
+        unordered_set<int>st;
+        for(int i=1;i<=bound;i*=x){
+            for(int j=1;i+j<=bound; j*=y){
+                st.insert(i+j);
 
-                if(y1 > bound)break;
-
-                long long val = (long long)x1 + y1;
-
-                if(val <= bound){
-                    ans.insert(val);
-                }else{
-                    break;
-                }
+                if(y==1)break;
             }
+            if(x==1)break;
         }
-        return vector<int>(ans.begin(),ans.end());
-    }
-
-    int binarySearch(int num, int bound){ 
-        int low = 0, high = bound;
-
-        while(low<=high){
-            long long mid = (low + high)/2;
-
-            if((long long)(mid*mid) <= bound){
-                low = mid + 1;
-            }else{
-                high = mid - 1;
-            }
-        }
-        return high;
+        return vector<int>(st.begin(),st.end());
     }
 };
