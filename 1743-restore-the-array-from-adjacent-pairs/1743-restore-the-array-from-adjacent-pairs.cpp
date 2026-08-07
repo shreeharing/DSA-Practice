80a14d099d43cacee2add719f00446e183da10fe
shreeharing <79448337+shreeharing@users.noreply.github.com>
2026-08-07 18:51:04 +0530

    Time: 268 ms (37.54%), Space: 131.2 MB (39.48%) - LeetHub

---
+++ 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> restoreArray(vector<vector<int>>& adjacentPairs) {
+        unordered_map<int,int>hashMap;
+        unordered_map<int,vector<int>>adjMap;
+        int n = adjacentPairs.size();
+
+        for(int i=0;i<n;i++){
+            hashMap[adjacentPairs[i][0]]++;
+            hashMap[adjacentPairs[i][1]]++;
+
+            adjMap[adjacentPairs[i][0]].push_back(adjacentPairs[i][1]);
+            adjMap[adjacentPairs[i][1]].push_back(adjacentPairs[i][0]);
+
+        }
+        int curEle=INT_MIN;
+
+        for(auto it:hashMap){
+            if(it.second==1){
+                curEle=it.first;
+                break;
+            }
+        }
+        vector<int>res;
+        int prevEle = INT_MIN;
+        while(res.size()<hashMap.size()){
+            if(prevEle != INT_MIN){
+                res.push_back(prevEle);
+            }
+            vector<int>temp=adjMap[curEle];
+            prevEle=curEle;
+            if(res.size()==0 || temp.size()==1 || res.back() != temp[0]){
+                curEle = temp[0];
+            }else{
+                curEle = temp[1];
+            }
+        }
+        return res;
+    }
+};
